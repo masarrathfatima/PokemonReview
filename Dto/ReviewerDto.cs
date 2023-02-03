fd6c10bba99bd716f4cb12f41cb3f937804262d1
masarrathfatima <Masarrathfatima2@gmail.com>
2023-02-03 13:56:23 +0530

    DTOs created

---
+++ Dto/ReviewerDto.cs
@@ -0,0 +1,14 @@
+﻿
+using PokemonReview.Models;
+
+namespace PokemonReview.Dto
+{
+    public class ReviewerDto
+    {
+       
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+}
