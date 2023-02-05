20fa4f0cdc12a8f3e119ff617c29a16199a2b879
masarrathfatima <Masarrathfatima2@gmail.com>
2023-02-05 11:52:29 +0530

    Project updated

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,180 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Dto;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+using PokemonReview.Repository;
+
+namespace PokemonReview.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository,
+            IOwnerRepository ownerRepository,
+            IReviewerRepository reviewerRepository,
+            IReviewRepository reviewRepository,
+            IMapper mapper)
+        {
+            _pokemonRepository = pokemonRepository;
+            _ownerRepository = ownerRepository;
+            _reviewerRepository = reviewerRepository;
+            _reviewRepository = reviewRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(ICollection<Pokemon>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+           
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+            return Ok(pokemons);
+        }
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId) 
+        {   
+            if(!_pokemonRepository.PokemonExists(pokeId)) 
+            {
+                return NotFound();
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+             var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
+            
+
+            
+            return Ok(pokemon);
+        }
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult PokemonRating(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState) ;
+            }
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            return Ok(rating);  
+        }
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreatePokemon([FromQuery] int ownerId, [FromQuery] int catId, [FromBody] PokemonDto pokemonCreate)
+        {
+            if (pokemonCreate == null)
+                return BadRequest(ModelState);
+
+
+            var pokemons = _pokemonRepository.GetPokemons()
+                .Where(c => c.Name.Trim().ToUpper() == pokemonCreate.Name.TrimEnd().ToUpper())
+                .FirstOrDefault();
+
+            if (pokemons != null)
+            {
+                ModelState.AddModelError("", "Pokemon already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+
+            var pokemonMap = _mapper.Map<Pokemon>(pokemonCreate);
+            
+            if (!_pokemonRepository.CreatePokemon(ownerId,catId,pokemonMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+
+            }
+            return Ok("Successfully Added");
+
+        }
+        [HttpPut("{pokeId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult UpdateCategory(int pokeId, 
+            [FromQuery]int ownerId, 
+            [FromQuery]int catId, 
+            [FromBody] PokemonDto updatePokemon)
+        {
+            if (updatePokemon == null)
+                return BadRequest(ModelState);
+            if (pokeId != updatePokemon.Id)
+                return BadRequest(ModelState);
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var pokemonMap = _mapper.Map<Pokemon>(updatePokemon);
+
+            if (!_pokemonRepository.UpdatePokemon(ownerId,catId,pokemonMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while Updating");
+                return StatusCode(500, ModelState);
+
+            }
+            return Ok(pokemonMap);
+        }
+        [HttpDelete("{pokeId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeletePokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+
+            var reviewsToDelete = _reviewRepository.GetReviewsOfAPokemon(pokeId);
+            var pokemonToDelete = _pokemonRepository.GetPokemon(pokeId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewRepository.DeleteReviews(reviewsToDelete.ToList()))
+            {
+                ModelState.AddModelError("", "Something went wrong when deleting reviews");
+            }
+
+            if (!_pokemonRepository.DeletePokemon(pokemonToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting owner");
+            }
+
+            return NoContent();
+        }
+
+
+
+
+
+
+    } 
+}
